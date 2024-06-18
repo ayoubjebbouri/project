67803196254dd5cae44fcbc36099d97c3d159cc6
Ayoub <ay.jebbouri@gmail.com>
2024-06-18 13:22:26 +0100

    change loggin condida and societe and add offer

--- src/main/java/com/exemple/web/DashboardSocieteController.java
+++ src/main/java/com/exemple/web/DashboardSocieteController.java
@@ -1,8 +1,14 @@
 package com.exemple.web;
 
 import com.exemple.dao.*;
+import com.exemple.entities.Deplom;
+import com.exemple.entities.Offer;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardSocieteController {
@@ -16,4 +22,18 @@ public class DashboardSocieteController {
     private CandidatureRepository candidatureRepository;
     @Autowired
     private AdminRepository adminRepository;
+
+    // serve static page
+    @GetMapping("/hommeaddoffer")
+    public String pageaddeploms(Model model){
+        model.addAttribute("offer", new Offer());
+        return "Admin/Addeplom";
+    }
+    // ADD OFFER
+    @GetMapping("/Addoffer")
+    public String addOffer(@Valid Offer offer, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) return "Societe/Addoffer";
+        offerRepository.save(offer);
+        return "Societe/Listeoffer";
+    }
 }
